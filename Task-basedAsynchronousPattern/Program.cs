cad2028da69d208888937425e57cbeb42d875f5d
Quoc-Minh Ton-That <tonthat.quocminh@gmail.com>
2018-01-01 13:52:01 -0500

    Added test class for AsyncIO threads

--- Task-basedAsynchronousPattern/Program.cs
+++ Task-basedAsynchronousPattern/Program.cs
@@ -19,9 +19,11 @@ namespace TaskbasedAsynchronousPattern
             //});
 
             //Long running option uses a background thread. I think it is slower than
-            //foreground thread.
-            TPL.Computation.RunCombinationsTestAsync(49000, 600/*, TaskCreationOptions.LongRunning*/);
-            TPL.Computation.RunCombinationsTest(49000, 600);
+            ////foreground thread.
+            //TPL.Computation.RunCombinationsTestAsync(49000, 600/*, TaskCreationOptions.LongRunning*/);
+            //TPL.Computation.RunCombinationsTest(49000, 600);
+
+            TPL.AsyncIO.RunTest();
 
             Console.WriteLine("All Done");
             Console.ReadKey();
---
+++ Task-basedAsynchronousPattern/TPL/AsyncIO.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TPL
+{
+    public class AsyncIO
+    {
+
+        #region Test Cases
+
+        public static void RunTest()
+        {
+            Task<string> downloadTask = DownloadWebPageAsync("https://www.google.ca/");
+
+            while (!downloadTask.IsCompleted)
+            {
+                Console.Write(".");
+                Thread.Sleep(250);
+            }
+
+            Console.WriteLine(downloadTask.Result);
+        }
+
+        #endregion
+
+        #region Custom Methods
+
+        private static Task<string> DownloadWebPageAsync(string url)
+        {
+            WebRequest request = WebRequest.Create(url);
+            IAsyncResult ar = request.BeginGetResponse(null, null);
+
+            Task<string> downloadTask =
+                Task.Factory
+                .FromAsync<string>(ar, iar =>
+                {
+                    using (WebResponse response = request.EndGetResponse(iar))
+                    {
+                        using (var reader = new StreamReader(response.GetResponseStream()))
+                        {
+                            return reader.ReadToEnd();
+                        }
+                    }
+                });
+
+            return downloadTask;
+        }
+
+        #endregion
+    }
+}
