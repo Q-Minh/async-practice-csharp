b0a419e767e3b442608c1410de3f18c6e302cd0f
Quoc-Minh Ton-That <tonthat.quocminh@gmail.com>
2018-01-03 12:47:30 -0500

    Started exploring Task relationships with continuations and nested Tasks

---
+++ TaskbasedAsynchronousPattern/TPL/TaskRelationships.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace TPL
+{
+    public class TaskRelationships
+    {
+        #region Test Cases
+
+        public static void RunChainedTaskTest()
+        {
+            SimpleContinuation();
+
+            TwoConditionalContinuation();
+        }
+
+        public static void RunNestedTaskTest()
+        {
+
+        }
+
+        #endregion
+
+        #region Custom Methods
+
+        private static void SimpleContinuation()
+        {
+            Task<int> firstTask = Task.Factory
+                      .StartNew<int>(() => { Console.WriteLine("First Task"); return 42; });
+
+            Task secondTask = firstTask
+                 .ContinueWith(ft => Console.WriteLine("Second Task, First task returned {0}", ft.Result));
+
+            secondTask.Wait();
+        }
+
+        private static void TwoConditionalContinuation()
+        {
+            Task<int> firstTask = Task.Factory
+                     .StartNew<int>(() => { Console.WriteLine("First Task"); return 42; });
+
+            Task secondTask = firstTask.ContinueWith(result => Console.WriteLine("Processed " + result),
+                                TaskContinuationOptions.OnlyOnRanToCompletion);
+
+            Task errorHandler = firstTask.ContinueWith(st => Console.WriteLine(st.Exception),
+                                            TaskContinuationOptions.OnlyOnFaulted);
+
+            secondTask.Wait();
+        }
+
+        #endregion
+
+    }
+}
